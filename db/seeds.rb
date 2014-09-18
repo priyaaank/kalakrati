ab98fcabadb35d6a1de0d160f693f61267503864
Priyank <priyaaank@gmail.com>
2014-09-18 22:32:40 +0530

    Creating some products, updating seed and adding mongohq config

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,20 @@
 #
 
 rose_bottle = Product.new(name: "Rose bottle with white flowers", description: "Rose bottle with white flowers", code: "Bottle13", price: 1200)
-rose_bottle.images << Image.new(url: "product/bottles/rose_bottle_with_white_flowers.jpg", primary: true)
+rose_bottle.images << Image.new(url: "product/paper/rose_bottle_with_white_flowers.jpg", primary: true)
 rose_bottle.save!
 
 brown_bottle = Product.new(name: "Brown bottle with flowers", description: "Brown bottle with flowers", code: "Bottle12", price: 1000)
-brown_bottle.images <<  Image.new(url: "product/bottles/brown_bottle_with_rope_1.jpg", primary: true)
-brown_bottle.images <<  Image.new(url: "product/bottles/brown_bottle_with_rope_2.jpg", primary: false)
+brown_bottle.images <<  Image.new(url: "product/paper/brown_bottle_with_rope_1.jpg", primary: true)
+brown_bottle.images <<  Image.new(url: "product/paper/brown_bottle_with_rope_2.jpg", primary: false)
 brown_bottle.save!
 
 blue_bottle = Product.new(name: "Blue bottles with gems", description: "Blue bottle with gems", code: "Bottle02", price: 900)
-blue_bottle.images <<  Image.new(url: "product/bottles/blue_bottle_with_gems_1.jpg", primary: true)
-blue_bottle.images <<  Image.new(url: "product/bottles/blue_bottle_with_gems_2.jpg", primary: false)
+blue_bottle.images <<  Image.new(url: "product/paper/blue_bottle_with_gems_1.jpg", primary: true)
+blue_bottle.images <<  Image.new(url: "product/paper/blue_bottle_with_gems_2.jpg", primary: false)
 blue_bottle.save!
 
 green_bottle = Product.new(name: "Green bottle with rope", description: "Green bottle with rope", code: "Bottle22", price: 500)
-green_bottle.images <<  Image.new(url: "product/bottles/green_bottle_with_rope_1.jpg", primary: true)
-green_bottle.images <<  Image.new(url: "product/bottles/green_bottle_with_rope_2.jpg", primary: false)
+green_bottle.images <<  Image.new(url: "product/paper/green_bottle_with_rope_1.jpg", primary: true)
+green_bottle.images <<  Image.new(url: "product/paper/green_bottle_with_rope_2.jpg", primary: false)
 green_bottle.save!
