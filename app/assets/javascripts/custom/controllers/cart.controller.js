7796334b7c4da475b227a55366a6359bce6e62cd
Priyank <priyaaank@gmail.com>
2014-09-26 18:15:48 +0530

    Fixing calculations on the cart page

--- app/assets/javascripts/custom/controllers/cart.controller.js
+++ app/assets/javascripts/custom/controllers/cart.controller.js
@@ -3,6 +3,15 @@ kalakratiApp.controller('CartController', ['$scope','$http', function($scope, $h
   $http.get('cart/items').success(function(data) {
     console.log(data);
     $scope.cart_items = data;
+
+      var totalPrice = 0.0;
+      var tax = 0.0;
+      for(var index in $scope.cart_items) {
+        totalPrice += ($scope.cart_items[index].price * $scope.cart_items[index].quantity);
+      }
+      $scope.product_total = totalPrice;
+      $scope.tax = tax
+      $scope.total_cost = $scope.product_total + $scope.tax;
   });
 
 }]);
