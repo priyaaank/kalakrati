bc858a0d398a485a247b6dbbacd5f2b5bc849b0f
Priyank <priyaaank@gmail.com>
2014-10-07 16:03:06 +0530

    Added flow for check out

--- app/assets/javascripts/custom/checkout/checkoutController.js
+++ app/assets/javascripts/custom/checkout/checkoutController.js
@@ -1,7 +1,13 @@
-kalakratiApp.controller('CheckoutController', ['$scope', '$location', '$window', 'CheckoutService', function($scope, $location, $window, CheckoutService) {
+kalakratiApp.controller('CheckoutController', ['$scope', '$location', '$window', 'CartService', 'CheckoutService', function($scope, $location, $window, CartService, CheckoutService) {
 
   $scope.address = CheckoutService.Address;
+  $scope.cart_items = CartService.CartItems;
 
+  var cartUpdateSubscription = $scope.$on('cart:itemsUpdated',function(event, data) {
+    $scope.cart_items = CartService.CartItems;
+  });
+
+  $scope.$on('$destroy', cartUpdateSubscription);
   $scope.submitAddressDetails = function(isFormValid) {
     if(isFormValid) {
       CheckoutService.SaveCheckoutAddress($scope.address).then(function() {
