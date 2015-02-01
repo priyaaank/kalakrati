0ccf4c27ffc83a5ffcd6cbb83581431fc306869b
Priyank Gupta <priyaaank@gmail.com>
2015-02-01 23:20:28 +0530

    Fixing template and addting notes output to email

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,8 @@ class Order
 
   include Mongoid::Document
 
+  field :tnc_acceptance, type: Boolean, default: false
+  field :payment_notes, type: String
   has_many :order_items
   embeds_one :address
 
@@ -12,6 +14,8 @@ class Order
     new_order.address = shopping_cart.address
     new_order.currency = shopping_cart.currency
     new_order.order_items = shopping_cart.shopping_cart_items.collect { |item| OrderItem.from_cart_item(item, new_order) }
+    new_order.tnc_acceptance = shopping_cart.tnc_acceptance
+    new_order.payment_notes = shopping_cart.payment_notes
     new_order.save!
     new_order
   end
