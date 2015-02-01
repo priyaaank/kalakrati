cef62b47cba2c818a38067cfaa4032cc4e9fe348
Priyank Gupta <priyaaank@gmail.com>
2015-02-01 22:51:38 +0530

    Fixed template errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.asset_host = 'http://kalakrati.herokuapp.com'
+  config.action_mailer.default_url_options = { host: 'kalakrati.herokuapp.com' }
   config.action_mailer.smtp_settings = {
       address:              'smtp.mandrillapp.com',
       port:                 '587',
