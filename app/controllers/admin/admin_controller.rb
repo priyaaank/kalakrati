434c1205fbc5e827f1bd0d58a48c31be0dc64199
Priyank Gupta <priyaaank@gmail.com>
2015-02-18 00:01:08 +0530

    Added cloudinary config yml

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,8 +3,7 @@ class Admin::AdminController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  after_filter :set_csrf_cookie_for_ng, :cors_set_access_control_headers
-  before_filter :cors_preflight_check
+  after_filter :set_csrf_cookie_for_ng
 
   layout 'admin'
 
@@ -16,26 +15,6 @@ class Admin::AdminController < ActionController::Base
 
   def verified_request?
     super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
-  end
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-# If this is a preflight OPTIONS request, then short-circuit the
-# request, return only the necessary headers and return an empty
-# text/plain.
-
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    end
   end
 
 end
