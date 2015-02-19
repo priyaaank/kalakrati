365b55b36fef41b4693140b373d2653449ba48bf
Priyank Gupta <priyaaank@gmail.com>
2015-02-19 22:38:01 +0530

    Add basic http auth for admin portal

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -4,6 +4,7 @@ class Admin::AdminController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   after_filter :set_csrf_cookie_for_ng
+  before_filter :authenticate
 
   layout 'admin'
 
@@ -11,10 +12,20 @@ class Admin::AdminController < ActionController::Base
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
   end
 
+  def logged_in?
+    not request.authorization.nil?
+  end
+
   protected
 
   def verified_request?
     super || form_authenticity_token == request.headers['X-XSRF-TOKEN']
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['ADMIN_ID'] && password == ENV['ADMIN_PASSWORD']
+    end
   end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,5 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
 end
