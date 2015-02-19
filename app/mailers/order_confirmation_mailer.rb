cb8e37f2ba164cc48aeb85a2db30e36d5ab2aa9a
Priyank Gupta <priyaaank@gmail.com>
2015-02-19 16:03:56 +0530

    Fixed the host name for different environments

--- app/mailers/order_confirmation_mailer.rb
+++ app/mailers/order_confirmation_mailer.rb
@@ -2,6 +2,7 @@ class OrderConfirmationMailer < ApplicationMailer
 
   def order_confirmation_email(order)
     @order = order
+    @website_host = ActionMailer::Base.default_url_options[:host]
     mail(
         to: order.address.email,
         subject: "Kalakrati: We recieved your order [#{order.id.to_s}] of ₹#{order.total_order_amount} from #{order.address.city}",
