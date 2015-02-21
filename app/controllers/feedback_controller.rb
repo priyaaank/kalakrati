50ebafc172149c1dd7ac62148b99212d21f3377d
Priyank Gupta <priyaaank@gmail.com>
2015-02-21 18:28:00 +0530

    Adding ability to send out emails when feedback comes in

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -3,7 +3,8 @@ class FeedbackController < ApplicationController
   skip_before_action :verify_authenticity_token, :only => [:create]
 
   def create
-    puts params.inspect
+    feedback = Feedback.new(params[:name], params[:email], params[:message])
+    FeedbackMailer.send_feedback(feedback).deliver
     render :status => 200, :nothing => true
   end
 
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,4 +2,10 @@ class Feedback
 
   attr_accessor :from_email, :from_name, :message
 
+  def initialize(name, email, message)
+    @from_email = email
+    @from_name = name
+    @message = message
+  end
+
 end
