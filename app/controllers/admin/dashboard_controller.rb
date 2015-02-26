16d542d995688f61de56cc87b2fa910c904c79c0
Priyank Gupta <priyaaank@gmail.com>
2015-02-26 22:38:41 +0530

    Making dashboard statistics a bit meaningful

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -4,6 +4,7 @@ class Admin::DashboardController < Admin::AdminController
     @order_count = Order.all.count
     @product_count = Product.all.count
     @shopping_cart_count = ShoppingCart.all.count
+    @abandoned_shopping_cart_count = ShoppingCart.where(:id.in => ShoppingCartItem.distinct(:shopping_cart_id)).count
     render :show
   end
 
