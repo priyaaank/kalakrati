bdc8add0560482b7102065c361bd48f2669b3a1c
Priyank Gupta <priyaaank@gmail.com>
2015-02-27 16:36:32 +0530

    Updated icons and color in order helper

--- app/helpers/admin/order_helper.rb
+++ app/helpers/admin/order_helper.rb
@@ -1,7 +1,7 @@
 module Admin::OrderHelper
 
-  STATE_ICON_MAP = {paid: "fa-credit-card", shipped: "fa-truck", delivered: "fa-check", cancelled: "fa-times", ordered: "fa-gift", refund: "fa-reply"}
-  STATE_ICON_COLOR = {paid: "primary", shipped: "info", delivered: "success", cancelled: "danger", ordered: "", refund: "warning"}
+  STATE_ICON_MAP = {paid: "fa-credit-card", shipped: "fa-truck", delivered: "fa-check", cancel: "fa-times", ordered: "fa-gift", refund: "fa-reply"}
+  STATE_ICON_COLOR = {paid: "primary", shipped: "info", delivered: "success", cancel: "danger", ordered: "", refund: "warning"}
   DEFAULT_ICON = "fa-info-circle"
   DEFAULT_COLOR = ""
 
